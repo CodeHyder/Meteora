db8577ecb27e57104b5d8c82ba3699c154ac90f4
CodeHyder <rapharodriguescm20@gmail.com>
2024-05-09 00:29:29 -0300

    <feature>: tentando alterar as imagens do carrossel dinamicamente de acordo com o tamanho da tela

--- src/components/Carrousels/index.jsx
+++ src/components/Carrousels/index.jsx
@@ -1,16 +1,46 @@
-import Carousel from 'react-bootstrap/Carousel'; 
-import styles from './Carrousels.module.css';
+import Carousel from 'react-bootstrap/Carousel';
+import styles from './Carrousels.module.css'; 
+import React, { useState, useEffect } from 'react';
+
+
 
 function Carrousels({ props }) {
-    return (
+    
+    const [imageSrcs] = useState({
+        desktop: props.map((item) => item.srcDesktop),
+        mobile: props.map((item) => item.srcMobile),
+        tablet: props.map((item) => item.srcTablet),
+      });
 
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const handleResize = () => {
+          setScreenWidth(window.innerWidth);
+        };
+    
+        window.addEventListener('resize', handleResize);
+    
+        return () => {
+          window.removeEventListener('resize', handleResize);
+        };
+      }, []);
+
+    return (
         <Carousel>
-            {props.map((item) => {
+            {props.map((item, index) => {
                 return (
-                    <Carousel.Item>
+                    
+                    <Carousel.Item key={index}>
                         <img
                             className={styles.bannerImg}
-                            src={item.src}
+                            src={
+                                screenWidth < 642 
+                                ? imageSrcs.mobile[index]
+                                : screenWidth < 915 
+                                ? imageSrcs.tablet[index]   
+                                : imageSrcs.desktop[index] 
+                            }
                             alt={item.alt}
                         />
                     </Carousel.Item>
--- src/data/carroulselData.jsx
+++ src/data/carroulselData.jsx
@@ -1,19 +1,25 @@
 export const carroulselData = [
     {
         id: 1,
-        src: '../../src/assets/Desktop/Banner1.png',
+        srcDesktop: '../../src/assets/Desktop/Banner1.png',
+        srcTablet: '../../src/assets/Tablet/Banner1tablet.png',
+        srcMobile: '../../src/assets/Mobile/Banner1mobile.png',
         alt: "Banner da loja METEORA. Há uma mulher parda a esquerda e a direita o logo da loja Meteora escrito Moda de Impacto Positivo."
 
     },
 
     {
         id: 2,
-        src: '../../src/assets/Desktop/Banner2.png',
+        srcDesktop: '../../src/assets/Desktop/Banner2.png',
+        srcTablet: '../../src/assets/Tablet/Banner2tablet.png',
+        srcMobile: '../../src/assets/Mobile/Banner2mobile.png',
         alt: "Uma imagem com um fundo cor de rosa bem claro. Há uma mulher negra a esquerda, vestindo roupas prestas com listras brancas e usando um óculos. "
     },
     {
         id: 3,
-        src: '../../src/assets/Desktop/Banner3.png',
+        srcDesktop: '../../src/assets/Desktop/Banner3.png',
+        srcTablet: '../../src/assets/Tablet/Banner3tablet.png',
+        srcMobile: '../../src/assets/Mobile/Banner3mobile.png',
         alt: "Uma imagem com um fundo laranja suave. Há um homem negro usando chapéu, óculos, colar e roupas brancas. "
     }
 ]

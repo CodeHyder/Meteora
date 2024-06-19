953d8d68e77332023fa7e183d45b8edba6e9e8a8
raphael <rapharodriguescm20@gmail.com>
2024-06-18 23:58:14 -0300

    criando menu hamburguer do mobile

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 Esse projeto é um desafio da escola ALURA. Ele está sendo desenvolvido por mim (Raphael) do zero. 
 
-Utilizei Bootstrap para implementar o carrossel e quanto ao CSS estou fazendo do jeito roots (agora eu entendo porque css-in-js é melhor)
+Utilizei Bootstrap para implementar o carrossel e quanto ao CSS estou fazendo do jeito roots (agora eu entendo porque css-in-js é melhor, mas tem sido um treino bacana)
 
-Figma do projeto: https://www.figma.com/design/2TLgt8UjsWUViWlmpXu5Fz/Challenge-Front-end-%7C-Loja-Meteora?node-id=2386-3188&t=HGAYqFafttypQS1O-0
-
+ Você pode consultar o figma do projeto [nesse link](<https://www.figma.com/design/2TLgt8UjsWUViWlmpXu5Fz/Challenge-Front-end-%7C-Loja-Meteora?node-id=2386-3188&t=HGAYqFafttypQS1O-0/>)
--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,31 +1,37 @@
 import logo from '../../assets/Logodesktop.png';
 import styles from './NavBar.module.css';
 import { data } from './data';
+import menuIcon from '../../assets/icon-menu.svg';
+import closeMenu from '../../assets/marca-cruzada.svg';
 
 function NavBar() {
     return (
         <nav className={styles.navBar}>
             <div className={styles.navBar__container}>
                 <div className={styles.navListContainer}>
-                    <img src={logo} alt="Logo da loja Meteora" className={styles.navBar__logo}/>
+                    <img src={logo} alt="Logo da loja Meteora" className={styles.navBar__logo} />
+                    <img src={menuIcon} alt='icone do menu' className={styles.menuIcon} />
                     <ul className={styles.navList}>
 
-                        {data.map((item) =>  {
+                        {data.map((item) => {
                             return (
-                                <li 
-                                className={styles.navList__item} 
-                                key={item.id}
-                                href={item.href}
-                                >
-                                    <a className={styles.navList__link}>{item.nome}</a>
-                                </li>
+                                <>
+                                    <li
+                                        className={styles.navList__item}
+                                        key={item.id}
+                                        href={item.href}
+                                    >
+                                        <a className={styles.navList__link}>{item.nome}</a>
+                                    </li>
+                                    <div className={styles.navListLine}></div>
+                                </>
                             )
                         })}
                     </ul>
                 </div>
 
                 <form action="" className={styles.navFormContainer}>
-                    <input type="text" name="" className={styles.navFormInput} placeholder='Digite o produto'/>
+                    <input type="text" name="" className={styles.navFormInput} placeholder='Digite o produto' />
                     <button className={styles.navFormButton}>Buscar</button>
                 </form>
             </div>

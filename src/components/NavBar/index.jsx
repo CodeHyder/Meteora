57acdf9b19157c34d9815f18b2d72d8c248fa617
raphael <rapharodriguescm20@gmail.com>
2024-07-17 18:59:57 -0300

    new feature: menu do mobile

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -2,17 +2,40 @@ import logo from '../../assets/Logodesktop.png';
 import styles from './NavBar.module.css';
 import { data } from './data';
 import menuIcon from '../../assets/icon-menu.svg';
-import closeMenu from '../../assets/marca-cruzada.svg';
+import closeMenu from '../../assets/fechar.png';
+import { useState } from 'react';
+
 
 function NavBar() {
+
+    const [isMenuOpen, setisMenuOpen] = useState(false);
+
+    const openHandler = () => {
+
+        if (isMenuOpen) {
+            setisMenuOpen(false)
+        } else {
+            setisMenuOpen(true)
+        }
+    }
+    // className={styles.menuIcon}
     return (
         <nav className={styles.navBar}>
             <div className={styles.navBar__container}>
                 <div className={styles.navListContainer}>
                     <img src={logo} alt="Logo da loja Meteora" className={styles.navBar__logo} />
-                    <img src={menuIcon} alt='icone do menu' className={styles.menuIcon} />
-                    <ul className={styles.navList}>
-
+                    <img src={menuIcon} alt='icone do menu'
+                        className={isMenuOpen ?
+                            styles.iconInvisible
+                            : `${styles.menuIcon} ${styles.iconVisible}`
+                        }
+                        onClick={() => openHandler()}
+                    />
+                    <ul className={
+                        isMenuOpen ?
+                        `${styles.navList} ${styles.menu__visible}`
+                        : `${styles.menu__hidden} ${styles.navList}`
+                    }>
                         {data.map((item) => {
                             return (
                                 <>
@@ -28,6 +51,11 @@ function NavBar() {
                             )
                         })}
                     </ul>
+                    <img src={closeMenu}
+                        className={
+                            isMenuOpen ?
+                            `${styles.closeMenu} ${styles.iconVisible}`
+                            : styles.iconInvisible} onClick={() => openHandler()} />
                 </div>
 
                 <form action="" className={styles.navFormContainer}>
@@ -39,4 +67,4 @@ function NavBar() {
     )
 }
 
-export default NavBar;
+export default NavBar
